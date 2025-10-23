2139f553dc8a7bbb95b554d7527b064af63ae6a3
ThienAnTruong(Andrew) <166311237+AndrewTr0612@users.noreply.github.com>
2025-10-23 23:19:17 +0000

    update setting.py

--- trydjango/src/trydjango/trydjango/settings.py
+++ trydjango/src/trydjango/trydjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-l&2*codb5qf!d+h9=8vciy%iy(fer%k$4=y8lc6n2ylq@zv*7*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Allow all hosts for development (dev container compatibility)
 
 
 # Application definition
